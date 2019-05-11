f82f93fe7d7c0ff57159a95257a0a9862059d613
Evgeny Petrov <groosha@protonmail.com>
2019-05-11 23:39:22 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pybase100',
+      version='0.1',
+      description='A Python version of Base100 encoding mechanism',
+      url='https://github.com/MasterGroosha/pybase100',
+      author='Evgeny Petrov',
+      author_email='groosha@protonmail.com',
+      license='The Unlicence',
+      packages=['pybase100'],
+      zip_safe=False)
